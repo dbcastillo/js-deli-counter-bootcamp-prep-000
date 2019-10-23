e6351881f981eee8f52fedde7bfc2dacb4bad07d
Derick Castillo <derick.castillo@flatironschool.com>
2019-10-23 18:47:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function takeANumber(line, name) {
+    line.push(name)
+    return (
+      `Welcome, ${name}. You are number ${line.length} in line.`
+      )
+}
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return (
+      "There is nobody waiting to be served!"
+      )
+  }
+  else {
+    return (
+      `Currently serving ${line.shift()}.`
+      )
+  }
+}
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return ("The line is currently empty.")
+  } else {
+    const currentLine = "The line is currently: "
+    const index = 0
+    line.forEach((person) => {
+      currentLine += `${index + 1}. ${line[index]}` 
+      
+    })
+    
+  }
+}
